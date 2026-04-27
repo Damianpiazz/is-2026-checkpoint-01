a0cb4f3a69bf1bb6d0aafa4d85f3e23b527f6a42
milagroscrespo <milagros.mili.crespo@gmail.com>
2026-04-27 12:21:39 -0300

    feat(frontend): agrega conexión con backend

--- frontend/html/app.js
+++ frontend/html/app.js
@@ -1,64 +1,58 @@
-const integrantes = [
-  {
-    nombre: "Milagros",
-    apellido: "Crespo",
-    legajo: "33352",
-    feature: "Feature 02 - Frontend",
-    servicio: "frontend",
-    estado: "activo"
-  },
-  {
-    nombre: "Manuela",
-    apellido: "Chanquia",
-    legajo: "33159",
-    feature: "Feature 03 - Backend",
-    servicio: "backend",
-    estado: "activo"
-  },
-  {
-    nombre: "Lucia",
-    apellido: "Meza",
-    legajo: "33693",
-    feature: "Feature 04 - Base de Datos",
-    servicio: "database",
-    estado: "activo"
-  },
-  {
-    nombre: "Damian",
-    apellido: "Piazza",
-    legajo: "33400",
-    feature: "Feature 01 - Coordinación",
-    servicio: "coordinacion",
-    estado: "activo"
-  },
-  {
-    nombre: "Martina",
-    apellido: "Garcia",
-    legajo: "33093",
-    feature: "Feature 05 - Portainer",
-    servicio: "portainer",
-    estado: "activo"
-  }
-];
-
 const tbody = document.querySelector("#tabla tbody");
-
-integrantes.forEach(i => {
-  const row = `
-    <tr>
-      <td>${i.nombre}</td>
-      <td>${i.apellido}</td>
-      <td>${i.legajo}</td>
-      <td>${i.feature}</td>
-      <td><span class="badge ${i.servicio}">${i.servicio}</span></td>
-      <td>${i.estado}</td>
-    </tr>
-  `;
-  tbody.innerHTML += row;
-});
-
-// Estado visual
 const status = document.getElementById("status");
-status.textContent = "Mock activo";
-status.classList.remove("fail");
-status.classList.add("ok");
+
+const API_URL = window.API_URL;
+
+// renderización de la tabla
+function render(data) {
+  tbody.innerHTML = "";
+
+  data.forEach(i => {
+    const row = `
+      <tr>
+        <td>${i.nombre}</td>
+        <td>${i.apellido}</td>
+        <td>${i.legajo}</td>
+        <td>${i.feature}</td>
+        <td><span class="badge ${i.servicio}">${i.servicio}</span></td>
+        <td>${i.estado}</td>
+      </tr>
+    `;
+    tbody.innerHTML += row;
+  });
+}
+
+function setStatus(text, ok = true) {
+  status.textContent = text;
+  status.classList.remove("ok", "fail");
+  status.classList.add(ok ? "ok" : "fail");
+}
+
+//conexión con back
+async function loadData() {
+  try {
+    const response = await fetch(`${API_URL}/api/team`);
+
+    if (!response.ok) {
+      throw new Error("Error en la respuesta del backend");
+    }
+
+    const data = await response.json();
+
+    render(data);
+    setStatus("Backend conectado", true);
+
+  } catch (error) {
+    console.error("Error:", error);
+
+    setStatus("Error conectando al backend", false);
+
+    tbody.innerHTML = `
+      <tr>
+        <td colspan="6">No se pudieron cargar los datos</td>
+      </tr>
+    `;
+  }
+}
+
+loadData();
